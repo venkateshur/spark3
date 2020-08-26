a77e03a1b9442e93faf3412fb03ac170268bdb41
venkateshur <40040164+venkateshur@users.noreply.github.com>
2020-08-27 00:26:05 +0530

    updated

--- Example1.scala
+++ Example1.scala
@@ -7,10 +7,10 @@ object Example1 extends App with SparkSessionProvider {
 
   import spark.implicits._
 
-  private final val props = Array("delta.optimizer", "delta.compat")
+  private final val props = Map("delta.optimizer" -> "true", "delta.compat" -> "true", "serilaization" -> "write", "delta.compat1" -> "false")
 
   val sampleDf = Seq(
-    ("table_properties", "delta.optimizer=true, delta.compat=false")
+    ("table_properties", "delta.optimizer=true, delta.compat=false, delta.compat1=false")
   ).toDF("properties", "values")
 
   val newDf = sampleDf.filter(col("properties") === "table_properties")
@@ -18,11 +18,18 @@ object Example1 extends App with SparkSessionProvider {
   val propsMap = newDf.select("values").collect().flatMap(_.toString
     .replaceAll("\\[", "").replaceAll("]", "").split(",").map{prop =>
     val split = prop.split("=")
-    (split(0), split(1))
+    (split(0).trim, split(1).trim)
   }).toMap
 
-  val propsToBeUpdated = props.filter(propToBeChecked => propsMap.getOrElse(propToBeChecked, "false").toBoolean)
-  val updated = propsToBeUpdated.map(prop => s"$prop=true").mkString(", ")
+  val propsToBeUpdated = props.map{case (key, value) =>  {
+    propsMap.contains(key) match {
+      case true if !propsMap(key).equalsIgnoreCase(value.trim) => Map(key -> value)
+      case false =>  Map(key -> value)
+      case _ =>   Map.empty[String, String]
+    }
+  }}.reduce(_ ++ _).map{case(key,value) => s"$key=$value"}.mkString(", ")
+
+  propsToBeUpdated
 
   //take this updated string and put in table properties command
 
