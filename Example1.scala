9274307849f49fb5b599ecd24e9aea28582b533d
venkateshur <40040164+venkateshur@users.noreply.github.com>
2020-09-02 16:03:15 +0530

    Add files via upload

--- Example1.scala
+++ Example1.scala
@@ -1,19 +1,31 @@
 package examples
 
-import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.functions._
 
-
+case class Props(properties: String, values: String)
+case class Props1(SeqNum: Long, properties: String, values: String)
 object Example1 extends App with SparkSessionProvider {
 
   import spark.implicits._
 
   private final val props = Map("delta.optimizer" -> "true", "delta.compat" -> "true", "serilaization" -> "write", "delta.compat1" -> "false")
-
+ val previousSeq = 1L
   val sampleDf = Seq(
+    ("table_properties", "delta.optimizer=true, delta.compat=false, delta.compat1=false"),
+    ("table_properties", "delta.optimizer=true, delta.compat=false, delta.compat1=false"),
     ("table_properties", "delta.optimizer=true, delta.compat=false, delta.compat1=false")
-  ).toDF("properties", "values")
+  ).toDF("properties", "values").as[Props]
 
-  val newDf = sampleDf.filter(col("properties") === "table_properties")
+  val ttt = addSeqNum(sampleDf, previousSeq).cache()
+  ttt.show()
+
+  val maxValue = ttt.select(max("SeqNum")).collect()(0).getLong(0)
+  println(maxValue)
+
+
+
+  /*val newDf = sampleDf.filter(col("properties") === "table_properties")
   val propsMap1 = newDf.select("values").collect()
   val propsMap = newDf.select("values").collect().flatMap(_.toString
     .replaceAll("\\[", "").replaceAll("]", "").split(",").map{prop =>
@@ -29,8 +41,16 @@ object Example1 extends App with SparkSessionProvider {
     }
   }}.reduce(_ ++ _).map{case(key,value) => s"$key=$value"}.mkString(", ")
 
-  propsToBeUpdated
+  propsToBeUpdated*/
 
   //take this updated string and put in table properties command
 
+  def getPreviousMaxSeqNum(dbName: String, tableName: String, columnName: String): Long = {
+    spark.read.table(s"$dbName.$tableName").select(max(columnName)).collect()(0).getLong(0)
+  }
+
+  def addSeqNum(inputDs: Dataset[Props], prevValue: Long): Dataset[Props1] = {
+    inputDs.rdd.zipWithIndex().mapPartitions{_.map{case(actualRow, idx) =>
+      Props1(idx + prevValue, actualRow.properties, actualRow.values)}}.toDS()
+  }
 }
